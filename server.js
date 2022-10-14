6cd08832d0786a5527f44c19cef8b6bf632c0041
Phillip Wong <deepelegant99@gmail.com>
2022-10-13 22:13:16 -0700

    Added a few routes

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const app = express();
 
+// Register view engine
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render('index')
-  console.log("Yo");
+  res.render("index", {
+    name: "Phillip",
+    address: "1755 Rocky Mountain Avenue, Milpitas, CA 95035",
+  });
+});
+app.get("/dad", (req, res) => {
+  res.render("index", {
+    name: "Sam",
+    address: "1755 Rocky Mountain Avenue, Milpitas, CA 95035",
+  });
+});
+app.get("/mom", (req, res) => {
+  res.render("index", {
+    name: "Mou Noi",
+    address: "1755 Rocky Mountain Avenue, Milpitas, CA 95035",
+  });
+});
+app.get("/morality", (req, res) => {
+  res.download("./downloads/morality.pdf");
 });
 app.listen(9000);
